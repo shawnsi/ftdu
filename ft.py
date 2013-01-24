d8df5dc8adc10e7e5d742451cfa5eff474137e12
Shawn Siefkas <shawn.siefkas@meredith.com>
2013-01-24 14:04:24 -0600

    Removing unneeded variables from ft.py

--- ft.py
+++ ft.py
@@ -5,15 +5,11 @@ import sys
 import magic
 
 path = sys.argv[1]
-current_file = ''
-total_bytes = 0
-totals = {}
 
 for root, dirs, files in os.walk(path):
     for filename in files:
         filepath = os.path.join(root, filename)
         if not os.path.islink(filepath):
-            current_file = filepath
             filetype = magic.from_file(filepath).split(',')[0]
             print '%s: %s' % (filepath, filetype)
 
